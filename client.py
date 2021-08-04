07328087806c8158761a27d7c775e18051a73735
Jean Carraro <jgcarraro@gmail.com>
2021-08-04 02:03:03 -0300

    criptografia implementada, mensagem lida da stdin

--- client.py
+++ client.py
@@ -5,12 +5,18 @@ import os
 from struct import pack, unpack
 
 from Crypto.Util.number import getRandomNBitInteger
-from dotenv import load_dotenv
+from Crypto.Cipher import DES
+from dotenv import load_dotenv, find_dotenv
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 HOST = os.getenv("HOST")  # Standard loopback interface address (localhost)
 PORT = int(os.getenv("PORT"))  # Port to listen on (non-privileged ports are > 1023)
+
+# Função para transformar o texto em um multiplo de 8 bytes
+def pad(text):
+    n = len(text) % 8
+    return text if n == 0 else  text + (b' ' * (8-n))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
@@ -28,5 +34,11 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     [server_key] = unpack("!I", data)
     # Criacao da chave compartilhada
     shared_key = (server_key ** my_key) % prime_base
-    print(shared_key)
-
+    # Transformando chave compartilhada em bytes    
+    shared_key = shared_key.to_bytes(8, 'little')
+    # Criação objeto de criptografia DES 
+    cipher = DES.new(shared_key)
+    msg = input('Digite sua mensagem: ')
+    encrypted_msg = cipher.encrypt(pad(msg.encode('utf-8')))
+    print('Enviado:', encrypted_msg)
+    s.send(encrypted_msg)
--- server.py
+++ server.py
@@ -4,10 +4,11 @@ import socket
 import os
 from struct import pack, unpack
 
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 from Crypto.Util.number import getPrime, getRandomNBitInteger
+from Crypto.Cipher import DES
 
-load_dotenv()
+load_dotenv(find_dotenv())
 
 HOST = os.getenv("HOST")  # Standard loopback interface address (localhost)
 PORT = int(os.getenv("PORT"))  # Port to listen on (non-privileged ports are > 1023)
@@ -37,8 +38,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         [client_key] = unpack("!I", data)
         # Criacao da chave compartilhada
         shared_key = (client_key ** my_key) % prime_base
-        print(shared_key) 
-        # while True:
-        #     data = conn.recv(32)
-        #     if not data:
-        #         break
+        # Transformando chave compartilhada em bytes  
+        shared_key = shared_key.to_bytes(8, 'little')
+        # Criação objeto de criptografia DES
+        cipher = DES.new(shared_key)
+
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            msg = cipher.decrypt(data).decode('utf-8')
+            print('Mensagem criptografada rececida: ', data)
+            print('Descriptografada: ', msg)     
