060a0aea85182c6ebad0f088c7665694d47c03b5
Luiz Cesar Clabond Almeida <luiz.clabond@rd2.ventures>
2021-08-04 21:14:44 -0300

    chore: adicionado logs e inserido gerador aleatorio de msg

--- client.py
+++ client.py
@@ -6,6 +6,7 @@ from struct import pack, unpack
 
 from Crypto.Util.number import getRandomNBitInteger
 from Crypto.Cipher import DES
+from pyjokes import get_joke
 from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
@@ -19,26 +20,41 @@ def pad(text):
     return text if n == 0 else  text + (b' ' * (8-n))
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    print("Iniciando conexao com servidor")
     s.connect((HOST, PORT))
+    print("Aguardando recebimento do numero base e primo")
     # Recebimento das chaves bases
     data = s.recv(64)
     [prime_base, base] = (unpack("!II", data))
+    print("Primo e base recebidos:{} (primo) e {} base".format(prime_base, base))
     # Criação da chave publica 
-    my_key = getRandomNBitInteger(20)
+    my_key = getRandomNBitInteger(15)
+    print("Gerado inteiro secreto:", my_key)
+    print("Gerando chave publica a partir da base e primo")
     public_key = (base ** my_key) % prime_base
+    print("Chave publica gerada:", public_key)
     # Envio da chave publica
+    print("Enviando chave publica ao servidor")
     byte_array = pack("!I", public_key)
     s.send(byte_array)
     # Recebimento da chave publica do servidor
+    print("Aguardando recebimento da chave publica do servidor")
     data = s.recv(32)
     [server_key] = unpack("!I", data)
+    print("Chave publica do servidor recebida:", server_key)
     # Criacao da chave compartilhada
+    print("Gerando chave compartilhada")
     shared_key = (server_key ** my_key) % prime_base
+    print("Chave compartilhada gerada:", shared_key)
     # Transformando chave compartilhada em bytes    
     shared_key = shared_key.to_bytes(8, 'little')
     # Criação objeto de criptografia DES 
     cipher = DES.new(shared_key)
-    msg = input('Digite sua mensagem: ')
+    print("Gerando mensagem aleatoria")
+    msg = get_joke(language='es', category= 'all')
+    print("Mensagem gerada:", msg)
     encrypted_msg = cipher.encrypt(pad(msg.encode('utf-8')))
-    print('Enviado:', encrypted_msg)
+    print("Mensagem criptografada:", encrypted_msg)
+    print("Enviando mensagem")
     s.send(encrypted_msg)
+    print("Mensagem enviada. Encerrando")
--- server.py
+++ server.py
@@ -18,35 +18,51 @@ PORT = int(os.getenv("PORT"))  # Port to listen on (non-privileged ports are > 1
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
-    conn, addr = s.accept()
-    with conn:
-        print("Conectado por", addr)
-        # Geracao das chaves
-        prime_base = getPrime(32)
-        base = getRandomNBitInteger(20)
-        my_key = getRandomNBitInteger(20)
-        # Envio das bases
-        byte_array = pack("!II", prime_base, base)
-        conn.send(byte_array)
-        # Criação da chave publica
-        public_key = (base ** my_key) % prime_base
-        # Envio da chave publica
-        byte_array = pack("!I", public_key)
-        conn.send(byte_array)
-        # Recebimento da chave publica do cliente
-        data = conn.recv(32)
-        [client_key] = unpack("!I", data)
-        # Criacao da chave compartilhada
-        shared_key = (client_key ** my_key) % prime_base
-        # Transformando chave compartilhada em bytes  
-        shared_key = shared_key.to_bytes(8, 'little')
-        # Criação objeto de criptografia DES
-        cipher = DES.new(shared_key)
+    while True:
+      print("Aguardando conexoes")
+      conn, addr = s.accept()
+      with conn:
+          print("Conectado por", addr)
+          # Geracao das chaves
+          prime_base = getPrime(32)
+          print("Gerado primo:", prime_base)
+          base = getRandomNBitInteger(15)
+          print("Gerado base:", base)
+          my_key = getRandomNBitInteger(15)
+          print("Gerado inteiro secreto:", my_key)
+          # Envio das bases
+          print("Enviando para cliente o numero primo e a base")
+          byte_array = pack("!II", prime_base, base)
+          conn.send(byte_array)
+          # Criação da chave publica
+          print("Gerando chave publica a partir da base e primo")
+          public_key = (base ** my_key) % prime_base
+          print("Chave publica gerada:", public_key)
+          # Envio da chave publica
+          print("Enviando chave publica ao cliente")
+          byte_array = pack("!I", public_key)
+          conn.send(byte_array)
+          # Recebimento da chave publica do cliente
+          print("Aguardando recebimento da chave publica do cliente")
+          data = conn.recv(32)
+          [client_key] = unpack("!I", data)
+          print("Chave publica do cliente recebida:", client_key)
+          # Criacao da chave compartilhada
+          print("Gerando chave compartilhada")
+          shared_key = (client_key ** my_key) % prime_base
+          print("Chave compartilhada gerada:", shared_key)
 
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            msg = cipher.decrypt(data).decode('utf-8')
-            print('Mensagem criptografada rececida: ', data)
-            print('Descriptografada: ', msg)     
+          # Transformando chave compartilhada em bytes  
+          shared_key = shared_key.to_bytes(8, 'little')
+          # Criação objeto de criptografia DES
+          cipher = DES.new(shared_key)
+
+          print("Aguardando mensagens do cliente")
+          while True:
+              data = conn.recv(1024)
+              if not data:
+                  break
+              print("Dados do cliente recebidos:", data)
+              print("Descriptogradando mensagem")
+              msg = cipher.decrypt(data).decode('utf-8')
+              print('Mensagem descriptografada:', msg)     
